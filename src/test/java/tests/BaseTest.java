fa6d2d56781db5077863a62d51f400dc0a8b7df3
marynanesterenko <marynanesterenko.qa@gmail.com>
2024-12-21 01:38:59 -0500

    Changed the location where we navigate to the URL, also added a driver reset logic in the cleanUp method to avoid reuse of session that was quit

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import pages.HomePage;
+import pages.SearchResultsPage;
+import utils.DriverUtils;
+import utils.PropertyReader;
+
+import java.lang.reflect.Method;
+
+public class BaseTest {
+
+    HomePage homePage;
+    SearchResultsPage searchResultPage;
+
+    @BeforeMethod
+    public void setUp(Method test) {
+        PropertyReader.initProperty();
+        DriverUtils.createDriver(test);
+
+        DriverUtils.getDriver().get(PropertyReader.getProperty("application.url")); // Navigate to URL here
+        homePage = new HomePage();
+        searchResultPage = new SearchResultsPage();
+    }
+
+    @AfterMethod
+    public void cleanUp() {
+        if (DriverUtils.getDriver() != null) {
+            DriverUtils.getDriver().quit();
+            DriverUtils.driver = null; // Reset driver to avoid reuse of quit session
+        }
+    }
+}
